2447864f637d49c7307684d2c925698f77b8ef56
wen <asta.duan@gmail.com>
2023-05-23 12:07:44 +0800

    feat:check network status

---
+++ lib/utils/network_reachable.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+const _reachabilityHosts = [
+  'apple.com',
+  'amazon.com',
+  'huawei.com',
+];
+
+
+
+class Reachability {
+  factory Reachability() => _singleton ??= Reachability._();
+
+  Reachability._();
+
+  static Reachability? _singleton;
+
+  int _hostIndex = 0;
+
+  Future<bool> startPing() async {
+    _hostIndex = _hostIndex % _reachabilityHosts.length;
+    final host = _reachabilityHosts[_hostIndex];
+    _hostIndex++;
+    final success = await _checkInternetConnection(host);
+    if (success) {
+    return true;
+    }
+    return false;
+  }
+
+  Future<bool> _checkInternetConnection(String host) async {
+    try {
+      final response = await http.head(Uri.https(host));
+      return response.statusCode == HttpStatus.ok;
+    } catch (_, __) {
+      return false;
+    }
+  }
+
+}
